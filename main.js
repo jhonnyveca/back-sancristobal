8155ac4beb34566a55703a436a79f02dbeb825b0
jhonnyveca <jhonnyvecadev@gmail.com>
2023-03-04 21:28:59 -0500

    update cors

--- main.js
+++ main.js
@@ -3,15 +3,24 @@ const cors = require('cors');
 require('dotenv').config();
 const Connection = require('./config/database');
 
-const corsOptions = {
-  optionsSuccessStatus: 200,
-  credentials: true,
-};
 const app = express();
 const port = process.env.PORT || 3000;
 
 //TODO : ---middlewares---
-app.use(cors(corsOptions));
+app.use(cors());
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:8084');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With,content-type'
+  );
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 app.use(express.json());
 //TODO: ---Rutas---
 app.use('/api/v1', require('./src/routes'));
