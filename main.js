84100770e8be142b8d9853676cccd335f78383e4
jhonnyveca <jhonnyvecadev@gmail.com>
2023-03-04 21:18:53 -0500

    correct cors

--- main.js
+++ main.js
@@ -3,11 +3,15 @@ const cors = require('cors');
 require('dotenv').config();
 const Connection = require('./config/database');
 
+const corsOptions = {
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
 const app = express();
 const port = process.env.PORT || 3000;
 
 //TODO : ---middlewares---
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 //TODO: ---Rutas---
 app.use('/api/v1', require('./src/routes'));
